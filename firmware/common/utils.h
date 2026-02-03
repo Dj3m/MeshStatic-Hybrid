c9618dc58d15fb81ca90f6539ed2600d741a36c5
Dj3m <68070532+Dj3m@users.noreply.github.com>
2026-02-03 14:50:13 +0300

    Update utils.h

--- firmware/common/utils.h
+++ firmware/common/utils.h
@@ -6,7 +6,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-#define MAC_STR_LEN 18 // Длина строки MAC: 17 символов + нулевой терминатор
+#define MAC_STR_LEN 18
 
 typedef enum {
     LOG_ERROR,
@@ -15,14 +15,11 @@ typedef enum {
     LOG_DEBUG
 } LogLevel;
 
-// Функции работы с MAC-адресами
 void mac_to_string(const uint8_t* mac, char* buffer);
 bool string_to_mac(const char* str, uint8_t* mac);
 bool is_broadcast_mac(const uint8_t* mac);
 bool compare_mac(const uint8_t* mac1, const uint8_t* mac2);
 void copy_mac(uint8_t* dest, const uint8_t* src);
-
-// Утилиты
 uint16_t fletcher16(const uint8_t* data, size_t len);
 void log_message(LogLevel level, const char* format, ...);
 const char* byte_to_binary(uint8_t value);
@@ -31,4 +28,4 @@ float clamp_float(float value, float min, float max);
 float lerp(float a, float b, float t);
 uint16_t simple_hash(const char* str);
 
-#endif // UTILS_H
+#endif
